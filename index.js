4751011a6aa8b2f78d1215457cc559cc52cf1529
jason-ngn <80885742+jason-ngn@users.noreply.github.com>
2021-07-27 14:00:35 +0700

    added the load commands function in index.js

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const loadSlashCommands = require('./slashCommands/load-slash-commands');
+
 client.on('ready', () => {
   console.log(`${client.user.tag} has logged in!`);
+
+  loadSlashCommands(client);
 });
 
 client.login(process.env.TOKEN);
